2c7f7a5a2eed933ffb6a790a516e673350bc1b96
Talha Qureshi <bmw215t@gmail.com>
2023-04-26 10:48:11 -0400

    Added color utility

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,6 @@ import runner.Runner;
 
 public class Main {
 	public static void main(String[] args) {
-		try {
-			Runner.main(args);
-
-			
-			throw new LoginException();
-		} catch (LoginException e) {
-
-		}
+		Runner.main(args);
 	}
 }
